deb18454957c95461fc1d68521c2ad252cced795
Germán Martínez <germaga92@gmail.com>
2018-03-20 14:24:46 +0100

    WA-257 Page component refactoring

--- app/components/Page/index.js
+++ app/components/Page/index.js
@@ -5,11 +5,13 @@ import styles from './index.css'
 
 const Page = (props) => (
   <div>
-    <Header
-      account={props.account}
-      logOut={props.logOut}
-      whitelist={props.whitelist}
-    />
+    {!props.withoutHeader &&
+      <Header
+        account={props.account}
+        logOut={props.logOut}
+        whitelist={props.whitelist}
+      />
+    }
 
     <div className={styles.container}>
       {props.children}
