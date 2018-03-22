fa3d9aee6678b25426c2c56fc56d802daa4154cf
Germán Martínez <germaga92@gmail.com>
2018-03-22 20:31:32 +0100

    WA-258 Remove account creation from DownloadApps

--- app/routes/DownloadApps/store/actions/createAccount.js
+++
@@ -1,7 +0,0 @@
-export const CREATE_ACCOUNT = 'CREATE_ACCOUNT'
-
-export const createAccount = (address, seed) => ({
-  type: CREATE_ACCOUNT,
-  address,
-  seed,
-})
--- app/routes/DownloadApps/store/actions/logOutAccount.js
+++
@@ -1,5 +0,0 @@
-export const LOGOUT_ACCOUNT = 'LOGOUT_ACCOUNT'
-
-export const logOutAccount = () => ({
-  type: LOGOUT_ACCOUNT
-})
