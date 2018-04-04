afa29bb8bc97dd7fdc0d6c69df05875bbe8d882c
Germán Martínez <germaga92@gmail.com>
2018-04-04 23:07:20 +0200

    WA-283 Update PairingProcess reducer to manage the accounts

--- app/routes/PairingProcess/store/reducers/index.js
+++ app/routes/PairingProcess/store/reducers/index.js
@@ -1,14 +1,30 @@
 import {
-  CREATE_ACCOUNT,
+  CREATE_RELAYER_ACCOUNT,
+  CREATE_2FA_ACCOUNT,
 } from '../actions'
 
 function account(state = {}, action) {
+  let newState
+
   switch (action.type) {
 
-    case CREATE_ACCOUNT:
+    case CREATE_RELAYER_ACCOUNT:
       return {
-        address: action.address,
-        seed: action.seed,
+        ...state,
+        relayer: {
+          address: action.address,
+          seed: action.seed,
+        }
+      }
+
+      case CREATE_2FA_ACCOUNT:
+      return {
+        ...state,
+        secondFA: {
+          address: action.address,
+          seed: action.seed,
+          hmac: action.hmac,
+        }
       }
 
     default:
