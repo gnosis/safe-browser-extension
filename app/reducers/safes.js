096115e0b91e47bd4beb372a0b1815c8c6160350
Germán Martínez <germaga92@gmail.com>
2018-04-08 12:55:49 +0200

    WA-283 Redux reducers to manage the Safes

---
+++ app/reducers/safes.js
@@ -0,0 +1,46 @@
+import { ADD_SAFE } from 'routes/PairingProcess/store/actions'
+import {
+  DELETE_SAFE,
+  SELECT_SAFE,
+} from 'routes/SafesList/store/actions'
+
+const initialState = {
+  currentSafe: undefined,
+  safes: [],
+}
+function safes(state = initialState, action) {
+  switch (action.type) {
+
+    case ADD_SAFE:
+      const newSafe = {
+        address: action.address,
+        connectionType: action.connectionType,
+      }
+      return {
+        ...state,
+        currentSafe: action.address,
+        safes: [
+          ...state.safes,
+          newSafe,
+        ],
+      }
+
+    case DELETE_SAFE:
+      let safes = state.safes.filter(address => (address !== action.address))
+      return {
+        ...state,
+        safes,
+      }
+
+    case SELECT_SAFE: ({
+      ...state,
+      currentSafe: action.address,
+    })
+
+    default:
+      return state
+
+  }
+}
+
+export default safes
--- app/routes/PairingProcess/store/reducers/index.js
+++ app/routes/PairingProcess/store/reducers/index.js
@@ -3,28 +3,34 @@ import {
   CREATE_2FA_ACCOUNT,
 } from '../actions'
 
-function account(state = {}, action) {
+const initalState = {
+  relayer: {},
+  secondFA: {}
+}
+function account(state = initalState, action) {
   let newState
 
   switch (action.type) {
 
     case CREATE_RELAYER_ACCOUNT:
+      const relayer = {
+        address: action.address,
+        seed: action.seed,
+      }
       return {
         ...state,
-        relayer: {
-          address: action.address,
-          seed: action.seed,
-        }
+        relayer
       }
 
-      case CREATE_2FA_ACCOUNT:
+    case CREATE_2FA_ACCOUNT:
+      const secondFA = {
+        address: action.address,
+        seed: action.seed,
+        hmac: action.hmac,
+      }
       return {
         ...state,
-        secondFA: {
-          address: action.address,
-          seed: action.seed,
-          hmac: action.hmac,
-        }
+        secondFA
       }
 
     default:
