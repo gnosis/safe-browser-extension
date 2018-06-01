cfee0bee5bc9cb2ae037bfb7fff3a53d110d24dd
Germán Martínez <germaga92@gmail.com>
2018-06-01 11:13:48 +0200

    WA-409 Password confirmation tests

---
+++ app/components/ConfirmPasswordForm/ConfirmPassword.test.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Enzyme, { shallow, mount, render } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+import ConfirmPasswordForm from './containers/ConfirmPasswordForm'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+const manageConfirmPassword = jest.fn()
+
+describe('Confirm Password Form Validation', () => {
+
+  test('It should return true if the password matches', () => {
+    const newPassword = 'qwertyu1'
+    const confirmPassword = ''
+    const props = {
+      confirmPassword,
+      manageConfirmPassword,
+      ready: false,
+    }
+
+    const component = shallow(<ConfirmPasswordForm {...props} />)
+
+    const inputPassword = component.dive().find('input')
+    inputPassword.simulate('change', { target: { value: newPassword } })
+    
+    expect(manageConfirmPassword).toHaveBeenCalled()
+  })
+
+  test('It should return false if the password doesn\'t match', () => {
+    const password = 'qwertyu1'
+    const confirmPassword = 'asdfasdf'
+    const props = {
+      confirmPassword,
+      manageConfirmPassword,
+      ready: false,
+    }
+
+    const component = shallow(<ConfirmPasswordForm {...props} />)
+    
+    const inputPassword = component.dive().find('input')
+    inputPassword.simulate('change', { target: { value: confirmPassword } })
+    
+    expect(manageConfirmPassword).toHaveBeenCalled()
+  })
+})
