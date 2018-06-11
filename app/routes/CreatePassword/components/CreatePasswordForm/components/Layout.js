b50e87cb98433813684d279484a68485fc6096ea
Germán Martínez <germaga92@gmail.com>
2018-06-11 10:46:29 +0200

    WA-432 Add styles to create password

--- app/routes/CreatePassword/components/CreatePasswordForm/components/Layout.js
+++ app/routes/CreatePassword/components/CreatePasswordForm/components/Layout.js
@@ -1,40 +1,52 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 
-import Page from 'components/Page'
-import styles from './index.css'
+import styles from 'assets/css/global.css'
+import warningImage from 'assets/images/warning.svg'
 
 class Layout extends Component {
+  prevent = (e) => {
+    e.preventDefault()
+  }
+
   render() {
     const {
       newPassword,
       error,
       updateNewPassword,
+      ready,
     } = this.props
+
+    const rowStyle = (error && error.row) ? styles.textGreen : styles.textRed
+    const numberLetterStyle = (error && error.number && error.letter) ? styles.textGreen : styles.textRed
+    const lengthStyle = (error && error.length) ? styles.textGreen : styles.textRed
+    const dataValidation = !ready && newPassword !== '' ? 'ERROR' : ''
 
     return (
       <React.Fragment>
-        <p className={styles.text}>Create the password</p>
-        <input
-          type='password'
-          placeholder='Password'
-          value={newPassword}
-          onChange={updateNewPassword}
-        />
-        <div>
-          <div className={(error && error.length) ? styles.correct : styles.wrong}>
-            <p>Minimum 8 characters</p>
-          </div>
-          <div className={(error && error.number) ? styles.correct : styles.wrong}>
-            <p>Minimum 1 number</p>
-          </div>
-          <div className={(error && error.letter) ? styles.correct : styles.wrong}>
-            <p>Minimum 1 letter</p>
-          </div>
-          <div className={(error && error.row) ? styles.correct : styles.wrong}>
-            <p>No more than 2 identical characters in a row</p>
-          </div>
-        </div>
+        <h1>Secure and encrypt your browser extension with a password</h1>
+        <span className={styles.warningPassword}>
+          <img src={warningImage} />
+          <p>Password is used to unlock the extension and confirm transactions. <strong>Don't share this password with others!</strong></p>
+        </span>
+        <form onSubmit={this.prevent}>
+          <span data-validation={dataValidation}>
+            <input
+              type='password'
+              placeholder='New password'
+              value={newPassword}
+              onChange={updateNewPassword}
+            />
+          </span>
+          <p className={newPassword === '' ? null : rowStyle}>
+            No more than 2 identical characters in a row
+          </p>
+          <p className={newPassword === '' ? null : numberLetterStyle}>
+            Password with at least 1 number and 1 letter
+          </p>
+          <p className={newPassword === '' ? null : lengthStyle}>
+            Use a minimum of 8 characters
+          </p>
+        </form>
       </React.Fragment>
     )
   }
--- app/routes/CreatePassword/components/Layout.js
+++ app/routes/CreatePassword/components/Layout.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import classNames from 'classnames/bind'
 
 import Page from 'components/Page'
 import CreatePasswordForm from './CreatePasswordForm/containers/CreatePasswordForm'
-import styles from './index.css'
+import styles from 'assets/css/global.css'
+
+const cx = classNames.bind(styles)
 
 class Layout extends Component {
   render() {
@@ -13,23 +16,34 @@ class Layout extends Component {
       ready,
     } = this.props
 
-    return (
-      <Page>
-        <CreatePasswordForm
-          newPassword={newPassword}
-          manageCreatePassword={manageCreatePassword}
-        />
-        {ready &&
-          <Link to={{
-            pathname: '/confirm-password',
-            state: {
-              password: newPassword,
-            }
-          }}>
-            <button>CONTINUE</button>
-          </Link>
+    const nextLink = ready
+      ? {
+        pathname: '/confirm-password',
+        state: {
+          password: newPassword,
         }
-        {!ready && <button>CONTINUE</button>}
+      }
+      : ''
+
+    return (
+      <Page page={styles.password1} simpleHeader>
+        <div className={styles.content}>
+          <CreatePasswordForm
+            newPassword={newPassword}
+            manageCreatePassword={manageCreatePassword}
+            ready={ready}
+          />
+        </div>
+        <footer>
+          <Link to='/welcome' className={cx(styles.btnBack, styles.active)}></Link>
+          <ul className={styles.stepperDots}>
+            <li className={styles.active}></li>
+            <li></li>
+          </ul>
+          <Link to={nextLink} className={cx(styles.btnNext, ready ? styles.active : null)}>
+            <p>Next</p>
+          </Link>
+        </footer>
       </Page>
     )
   }
