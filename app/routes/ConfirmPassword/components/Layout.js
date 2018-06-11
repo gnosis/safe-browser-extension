e43a2409f721f82fa586cb6ae30a1132ddf757f8
Germán Martínez <germaga92@gmail.com>
2018-06-11 16:24:31 +0200

    WA-432 Add styles to confirm password

--- app/routes/ConfirmPassword/components/Layout.js
+++ app/routes/ConfirmPassword/components/Layout.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import classNames from 'classnames/bind'
 
 import Page from 'components/Page'
 import ConfirmPasswordForm from './ConfirmPasswordForm/containers/ConfirmPasswordForm'
-import styles from './index.css'
+import styles from 'assets/css/global.css'
+
+const cx = classNames.bind(styles)
 
 class Layout extends Component {
   render() {
@@ -14,22 +17,32 @@ class Layout extends Component {
       ready,
     } = this.props
 
+    const nextLink = ready
+      ? {
+        pathname: '/download-apps',
+        state: properties
+      }
+      : ''
+
     return (
-      <Page>
-        <ConfirmPasswordForm
-          confirmPassword={confirmPassword}
-          manageConfirmPassword={manageConfirmPassword}
-          ready={ready}
-        />
-        {ready &&
-          <Link to={{
-            pathname: '/pairing',
-            state: properties
-          }}>
-            <button>CONTINUE</button>
+      <Page page={styles.password2} simpleHeader>
+        <div className={styles.content}>
+          <ConfirmPasswordForm
+            confirmPassword={confirmPassword}
+            manageConfirmPassword={manageConfirmPassword}
+            ready={ready}
+          />
+        </div>
+        <footer>
+          <Link to='/create-password' className={cx(styles.btnBack, styles.active)}></Link>
+          <ul className={styles.stepperDots}>
+            <li></li>
+            <li className={styles.active}></li>
+          </ul>
+          <Link to={nextLink} className={cx(styles.btnNext, ready ? styles.active : null)}>
+            <p>Next</p>
           </Link>
-        }
-        {!ready && <button>CONTINUE</button>}
+        </footer>
       </Page>
     )
   }
