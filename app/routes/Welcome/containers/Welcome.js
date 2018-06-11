c7b36ae83b4fab88bd6eb8a22c7241f7a8e5eedd
Germán Martínez <germaga92@gmail.com>
2018-06-11 10:17:42 +0200

    WA-432 Add styles to welcome page

--- app/routes/Welcome/containers/Welcome.js
+++ app/routes/Welcome/containers/Welcome.js
@@ -1,10 +1,31 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react'
 
 import Layout from '../components/Layout'
 
-const Welcome = () => (
-  <Layout />
-)
+class Welcome extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      showDisclaimer: false,
+    }
+  }
+
+  toggleDisclaimer = () => {
+    const { showDisclaimer } = this.state
+    this.setState({ showDisclaimer: !showDisclaimer })
+  }
+
+  render() {
+    const { showDisclaimer } = this.state
+
+    return (
+      <Layout
+        showDisclaimer={showDisclaimer}
+        toggleDisclaimer={this.toggleDisclaimer}
+      />
+    )
+  }
+}
 
 export default Welcome
