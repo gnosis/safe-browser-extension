e276c21ec7902e977e8ba193051624fbca079435
Germán Martínez <germaga92@gmail.com>
2018-07-02 09:33:28 +0200

    Delete settings page

--- app/routes/Settings/containers/selector.js
+++
@@ -1,8 +0,0 @@
-import { createStructuredSelector } from 'reselect'
-import {
-  hasLockedAccountSelector,
-} from 'routes/DownloadApps/components/PairingProcess/store/selectors'
-
-export default createStructuredSelector({
-  hasLockedAccount: hasLockedAccountSelector,
-})
