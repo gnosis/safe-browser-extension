8fbe9a3ffa2db1b61dc3c48c5938d24d23c3f8c9
Germán Martínez <germaga92@gmail.com>
2018-07-10 09:58:43 +0200

    WA-408 Add get transaction hash

--- app/routes/Transaction/components/SendTransaction/containers/gasData.js
+++ app/routes/Transaction/components/SendTransaction/containers/gasData.js
@@ -37,3 +37,32 @@ export const getGasEstimation = (
       console.error(err)
     })
 }
+
+export const getTxHash = (tx, safeAddress) => {
+  const contract = TruffleContract(GnosisSafePersonalEdition)
+  const provider = new Web3.providers.HttpProvider(
+    config.networks[config.currentNetwork].url
+  )
+  contract.setProvider(provider)
+
+  return contract.at(safeAddress)
+    .then((instance) => {
+      return instance.getTransactionHash.call(
+        tx.to,
+        tx.value,
+        tx.data,
+        tx.operation,
+        tx.txGas,
+        tx.dataGas,
+        tx.gasPrice,
+        tx.gasToken,
+        tx.nonce,
+      )
+    })
+    .then((hash) => {
+      return hash
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+}
