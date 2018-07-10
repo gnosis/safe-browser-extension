5ed88497a861678cca7960fa9c4ea75c1cbb1553
Germán Martínez <germaga92@gmail.com>
2018-07-10 12:36:27 +0200

    WA-408 SendTransaction component

---
+++ app/routes/Transaction/components/SendTransaction/containers/selector.js
@@ -0,0 +1,10 @@
+import { createStructuredSelector } from 'reselect'
+import {
+  safesSelector,
+  transactionsSelector,
+} from 'routes/Transaction/store/selectors'
+
+export default createStructuredSelector({
+  safes: safesSelector,
+  transactions: transactionsSelector,
+})
