8c19ac730d8b71b0fd62173756a3e6375450037a
Germán Martínez <germaga92@gmail.com>
2018-12-10 13:17:21 +0100

    Refactor pop-up footer

---
+++ app/components/Footers/FooterButtons/containers/index.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react'
+import classNames from 'classnames'
+import { Redirect } from 'react-router'
+import { connect } from 'react-redux'
+
+import NetworkNotification from 'components/Notification/NetworkNotification'
+import styles from 'assets/css/global.css'
+import { PASSWORD_URL } from 'routes/routes'
+import selector from './selector'
+
+const cx = classNames.bind(styles)
+
+class FooterButtons extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      requestResolved: false
+    }
+  }
+
+  handleRejection = () => {
+    const {
+      handleRejection,
+      account
+    } = this.props
+
+    if (account.lockedState) {
+      this.setState({ requestResolved: true })
+    } else {
+      handleRejection()
+    }
+  }
+
+  handleConfirmation = () => {
+    const {
+      handleConfirmation,
+      account
+    } = this.props
+
+    if (account.lockedState) {
+      this.setState({ requestResolved: true })
+    } else {
+      handleConfirmation()
+    }
+  }
+
+  render () {
+    const {
+      account,
+      nextUrl,
+      rejectionText,
+      confirmationText
+    } = this.props
+    const { requestResolved } = this.state
+
+    if (requestResolved && account.lockedState) {
+      const passwordUrl = {
+        pathname: PASSWORD_URL,
+        state: {
+          dest: nextUrl
+        }
+      }
+      return <Redirect to={passwordUrl} />
+    }
+
+    return (
+      <NetworkNotification>
+        <span className={styles.buttonGroup}>
+          <button
+            onClick={this.handleRejection}
+            className={cx(styles.button, styles.reject)}
+          >
+            {rejectionText}
+          </button>
+          <button
+            onClick={this.handleConfirmation}
+            className={cx(styles.button, styles.confirm)}
+          >
+            {confirmationText}
+          </button>
+        </span>
+      </NetworkNotification>
+    )
+  }
+}
+
+export default connect(
+  selector
+)(FooterButtons)
