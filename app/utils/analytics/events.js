7b30038e4ee33f7b83764c50eccc72b5141ef8ee
Germán Martínez <germaga92@gmail.com>
2019-02-28 14:58:09 +0100

    Add signatures new event for Google Analytics

--- app/utils/analytics/events.js
+++ app/utils/analytics/events.js
@@ -3,8 +3,4 @@ export const ONBOARDING = 'ONBOARDING'
 export const SAFES = 'SAFES'
 export const EXTENSION_SETTINGS = 'EXTENSION SETTINGS'
 export const TRANSACTIONS = 'TRANSACTIONS'
-
-/*
-Pending events:
-ga(['_trackEvent', TRANSACTIONS, 'click-reload-transaction-data', 'Reload transaction data'])
-*/
+export const SIGNATURES = 'SIGNATURES'
