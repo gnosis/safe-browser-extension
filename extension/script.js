ad44b539f549d061769837d70f06d566579415e5
Germán Martínez <germaga92@gmail.com>
2019-03-25 11:39:08 +0200

    Remove comments

--- extension/script.js
+++ extension/script.js
@@ -15,36 +15,3 @@ const safeProvider = new SafeProvider({
 
 window.ethereum = safeProvider
 window.web3 = new Web3(safeProvider)
-
-/*
-window.addEventListener('load', () => {
-  console.log('loaded')
-  if (window.ethereum) {
-    // Modern dapp browsers...
-    window.web3 = new Web3(window.ethereum)
-    console.log('window.ethereum')
-    try {
-      // Request account access if needed
-      console.log('enable()')
-      window.ethereum.enable().then((result) => {
-        // Acccounts now exposed
-        // window.web3.eth.sendTransaction({ ... })
-        console.log('enabled!', result)
-      })
-    } catch (error) {
-      console.log('error', error)
-      // User denied account access...
-    }
-  } else if (window.web3) {
-    // Legacy dapp browsers...
-
-    window.web3 = new Web3(window.web3.currentProvider)
-    // Acccounts always exposed
-    // window.web3.eth.sendTransaction({ ... })
-    console.log('window.web3')
-  } else {
-    // Non-dapp browsers...
-    console.log('empty')
-  }
-})
-*/
