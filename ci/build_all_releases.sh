deec9777acb95122d243b00b6e20584d3b519df9
Germán Martínez <germaga92@gmail.com>
2019-04-23 14:13:48 +0200

    Update management of env variables

--- ci/build_all_releases.sh
+++ ci/build_all_releases.sh
@@ -14,28 +14,28 @@ fi
 
 # STAGING: Build and zip Rinkeby version
 npm run build:staging
-zip -r ./ci/outputs/staging-rinkeby.zip ./build/*
+zip -r ./ci/outputs/safe-browser-extension-$TRAVIS_BUILD_NUMBER-staging-rinkeby.zip ./build/*
 
 rm -r ./build
 
 # PRE-PRODUCTION: Build and zip Rinkeby version
 npm run build:preprod
-zip -r ./ci/outputs/preprod-rinkeby.zip ./build/*
+zip -r ./ci/outputs/safe-browser-extension-$TRAVIS_BUILD_NUMBER-preprod-rinkeby.zip ./build/*
 
 rm -r ./build
 
 # PRE-PRODUCTION: Build and zip Mainnet version
 npm run build:preprod-mainnet
-zip -r ./ci/outputs/preprod-mainnet.zip ./build/*
+zip -r ./ci/outputs/safe-browser-extension-$TRAVIS_BUILD_NUMBER-preprod-mainnet.zip ./build/*
 
 rm -r ./build
 
 # PRODUCTION: Build and zip Rinkeby version
 npm run build:prod
-zip -r ./ci/outputs/prod-rinkeby.zip ./build/*
+zip -r ./ci/outputs/safe-browser-extension-$TRAVIS_BUILD_NUMBER-rinkeby.zip ./build/*
 
 rm -r ./build
 
 # PRODUCTION: Build and zip Mainnet version
 npm run build:prod-mainnet
-zip -r ./ci/outputs/prod-mainnet.zip ./build/*
+zip -r ./ci/outputs/safe-browser-extension-$TRAVIS_BUILD_NUMBER-mainnet.zip ./build/*
--- scripts/build.js
+++ scripts/build.js
@@ -1,22 +1,23 @@
 const path = require('path')
 const fs = require('fs')
-const dotenv = require('dotenv')
 const manifest = require(path.resolve(__dirname, '../config/manifest_template.json'))
 const config = require(path.resolve(__dirname, '../config'))
 const names = require(path.resolve(__dirname, '../config/names'))
-const env = dotenv.config().parsed
 
 const buildDirectory = './build/'
 
 export const generateEnvVariables = () => {
-  const envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next])
-    return prev
-  }, {})
-  envKeys['process.env.NODE_ENV'] = process.env.NODE_ENV ? `"${process.env.NODE_ENV}"` : `"${names.DEVELOPMENT}"`
-  envKeys['process.env.NETWORK'] = process.env.NETWORK ? `"${process.env.NETWORK}"` : `"${names.RINKEBY}"`
+  const envKeys = Object
+    .keys(process.env)
+    .reduce((env, key) => {
+      env[key] = JSON.stringify(process.env[key])
+      return env
+    }, {
+      'NODE_ENV': process.env.NODE_ENV ? JSON.stringify(process.env.NODE_ENV) : JSON.stringify(names.DEVELOPMENT),
+      'NETWORK': process.env.NETWORK ? JSON.stringify(process.env.NETWORK) : JSON.stringify(names.RINKEBY)
+    })
 
-  return envKeys
+  return { 'process.env': envKeys }
 }
 
 export const generateManifestFile = () => {
