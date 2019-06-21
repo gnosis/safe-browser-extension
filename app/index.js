0f356f897e7bfe632207f63ee8bc6251189611f9
Germán Martínez <germaga92@gmail.com>
2019-06-21 08:58:35 +0200

    Check browser compatibility

--- app/index.js
+++ app/index.js
@@ -3,18 +3,19 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { Route } from 'react-router-dom'
 import { ConnectedRouter } from 'react-router-redux'
-
 import { ExtensionRoutes } from 'routes'
 import {
   ACCOUNT_URL,
   PASSWORD_URL,
   DOWNLOAD_APPS_URL,
-  WELCOME_URL
+  WELCOME_URL,
+  BLOCK_ACCESS_URL
 } from 'routes/routes'
 import { history, store } from './store'
 import { withAnalytics } from 'utils/analytics'
+import { isBrowserCompatible } from 'logic/browser/browserCompatibility'
 
-const calculateInitialUrl = (account, safes, device) => {
+const calculateInitialUrl = async (account, safes, device) => {
   const validAccount = account.secondFA && account.secondFA.seed
   const validSafes = safes.safes && safes.safes.length > 0
   const lastUpdateNotified = device.lastUpdateNotified
@@ -31,6 +32,11 @@ const calculateInitialUrl = (account, safes, device) => {
     return ACCOUNT_URL
   }
 
+  const browserCompatible = await isBrowserCompatible()
+  if (!browserCompatible) {
+    return BLOCK_ACCESS_URL
+  }
+  
   if (validAccount && !validSafes) {
     return {
       pathname: PASSWORD_URL,
@@ -59,7 +65,7 @@ store.ready().then(async () => {
 
   const { account, safes, device } = store.state
 
-  const url = calculateInitialUrl(account, safes, device)
+  const url = await calculateInitialUrl(account, safes, device)
   history.push(url)
 
   ReactDOM.render(
