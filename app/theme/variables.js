2c515a51aeef6834e087157f1b771a1858bc751f
Germán Martínez <germaga92@gmail.com>
2019-07-25 12:58:00 +0200

    Add new color variables

--- app/theme/variables.js
+++ app/theme/variables.js
@@ -3,9 +3,11 @@ const lightGrey = '#b2b5b2'
 const grey = '#5d6d74'
 const darkGrey = '#e9e7e6'
 const darkBlue = '#001428'
+const darkBlueTitle = '#001b31'
 const green = '#008c73'
 const red = '#f02525'
 const white = '#ffffff'
+const black = '#000000'
 
 module.exports = Object.assign(
   {},
@@ -15,8 +17,10 @@ module.exports = Object.assign(
     grey,
     darkGrey,
     darkBlue,
+    darkBlueTitle,
     green,
     red,
     white,
+    black,
   }
 )
