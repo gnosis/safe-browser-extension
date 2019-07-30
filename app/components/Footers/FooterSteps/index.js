5e5a123505e3a6af16d2231c1e6b3b1305942322
Germán Martínez <germaga92@gmail.com>
2019-07-30 17:41:56 +0200

    Refactor footer with stepper

--- app/components/Footers/FooterSteps/index.js
+++ app/components/Footers/FooterSteps/index.js
@@ -1,50 +1,41 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import classNames from 'classnames/bind'
 import { Redirect } from 'react-router'
-import styles from 'assets/css/global.css'
+import Button from 'components/layout/Button'
 import { NEXT } from '../../../../config/messages'
+import styles from './style.css'
 
 const cx = classNames.bind(styles)
 
-class FooterSteps extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      actionURL: ''
-    }
+const FooterSteps = ({ isReady, firstStep, secondStep, link, nextLink }) => {
+  const [actionURL, setActionURL] = useState(null)
+
+  const handleButton = (link) => (e) => {
+    setActionURL(link)
   }
 
-  handleButton = (link) => (e) => {
-    this.setState({ actionURL: link })
+  if (actionURL) {
+    return <Redirect to={actionURL} />
   }
-
-  render = () => {
-    const { ready, firstStep, secondStep, link, nextLink } = this.props
-    const { actionURL } = this.state
-
-    if (actionURL !== '') {
-      return <Redirect to={actionURL} />
-    }
-    return (
-      <footer>
-        <button
-          onClick={this.handleButton(link)}
-          className={cx(styles.btnBack, styles.active)}
-          type="button"
-        />
-        <ul className={styles.stepperDots}>
-          <li className={firstStep && styles.active} />
-          <li className={secondStep && styles.active} />
-        </ul>
-        <button
-          onClick={this.handleButton(nextLink)}
-          className={cx(styles.btnNext, ready && styles.active)}
-        >
-          <p>{NEXT}</p>
-        </button>
-      </footer>
-    )
-  }
+  return (
+    <footer>
+      <Button
+        onClick={handleButton(link)}
+        className={cx(styles.button, styles.btnBack, styles.active)}
+        type="button"
+      />
+      <ul className={styles.stepperDots}>
+        <li className={firstStep && styles.active} />
+        <li className={secondStep && styles.active} />
+      </ul>
+      <Button
+        onClick={handleButton(nextLink)}
+        className={cx(styles.button, styles.btnNext, isReady && styles.active)}
+      >
+        <p>{NEXT}</p>
+      </Button>
+    </footer>
+  )
 }
 
 export default FooterSteps
