0109571ab8872c308b8ab763145a9a67f14f554f
Germán Martínez <germaga92@gmail.com>
2019-08-15 02:15:45 +0200

    Update scripts to build and zip the extensions

--- ci/build_all_releases.sh
+++ ci/build_all_releases.sh
@@ -14,28 +14,25 @@ fi
 
 # STAGING: Build and zip Rinkeby version
 npm run build:staging
-zip -r ./ci/outputs/safe-browser-extension-$TRAVIS_BUILD_NUMBER-staging-rinkeby.zip ./build/*
-
+cd ./build && zip -r ../ci/outputs/safe-browser-extension-$TRAVIS_BUILD_NUMBER-staging-rinkeby.zip * && cd ..
 rm -r ./build
 
 # PRE-PRODUCTION: Build and zip Rinkeby version
 npm run build:preprod
-zip -r ./ci/outputs/safe-browser-extension-$TRAVIS_BUILD_NUMBER-preprod-rinkeby.zip ./build/*
-
+cd ./build && zip -r ../ci/outputs/safe-browser-extension-$TRAVIS_BUILD_NUMBER-preprod-rinkeby.zip * && cd ..
 rm -r ./build
 
 # PRE-PRODUCTION: Build and zip Mainnet version
 npm run build:preprod-mainnet
-zip -r ./ci/outputs/safe-browser-extension-$TRAVIS_BUILD_NUMBER-preprod-mainnet.zip ./build/*
-
+cd ./build && zip -r ../ci/outputs/safe-browser-extension-$TRAVIS_BUILD_NUMBER-preprod-mainnet.zip * && cd ..
 rm -r ./build
 
 # PRODUCTION: Build and zip Rinkeby version
 npm run build:prod
-zip -r ./ci/outputs/safe-browser-extension-$TRAVIS_BUILD_NUMBER-rinkeby.zip ./build/*
-
+cd ./build && zip -r ../ci/outputs/safe-browser-extension-$TRAVIS_BUILD_NUMBER-rinkeby.zip * && cd ..
 rm -r ./build
 
 # PRODUCTION: Build and zip Mainnet version
 npm run build:prod-mainnet
-zip -r ./ci/outputs/safe-browser-extension-$TRAVIS_BUILD_NUMBER-mainnet.zip ./build/*
+cd ./build && zip -r ../ci/outputs/safe-browser-extension-$TRAVIS_BUILD_NUMBER-mainnet.zip * && cd ..
+rm -r ./build
