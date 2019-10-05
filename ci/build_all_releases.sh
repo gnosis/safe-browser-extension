4c6a1ce0869da7e4fdf6b5b6f01f8e9a45dec13f
Germán Martínez <germaga92@gmail.com>
2019-10-05 06:59:12 +0200

    Update built packages names

--- ci/build_all_releases.sh
+++ ci/build_all_releases.sh
@@ -14,25 +14,25 @@ fi
 
 # STAGING: Build and zip Rinkeby version
 npm run build:staging
-cd ./build && zip -r ../ci/outputs/safe-browser-extension-$TRAVIS_BUILD_NUMBER-staging-rinkeby.zip * && cd ..
+cd ./build && zip -r ../ci/outputs/gnosis-safe-authenticator-$TRAVIS_BUILD_NUMBER-staging-rinkeby.zip * && cd ..
 rm -r ./build
 
 # PRE-PRODUCTION: Build and zip Rinkeby version
 npm run build:preprod
-cd ./build && zip -r ../ci/outputs/safe-browser-extension-$TRAVIS_BUILD_NUMBER-preprod-rinkeby.zip * && cd ..
+cd ./build && zip -r ../ci/outputs/gnosis-safe-authenticator-$TRAVIS_BUILD_NUMBER-preprod-rinkeby.zip * && cd ..
 rm -r ./build
 
 # PRE-PRODUCTION: Build and zip Mainnet version
 npm run build:preprod-mainnet
-cd ./build && zip -r ../ci/outputs/safe-browser-extension-$TRAVIS_BUILD_NUMBER-preprod-mainnet.zip * && cd ..
+cd ./build && zip -r ../ci/outputs/gnosis-safe-authenticator-$TRAVIS_BUILD_NUMBER-preprod-mainnet.zip * && cd ..
 rm -r ./build
 
 # PRODUCTION: Build and zip Rinkeby version
 npm run build:prod
-cd ./build && zip -r ../ci/outputs/safe-browser-extension-$TRAVIS_BUILD_NUMBER-rinkeby.zip * && cd ..
+cd ./build && zip -r ../ci/outputs/gnosis-safe-authenticator-$TRAVIS_BUILD_NUMBER-rinkeby.zip * && cd ..
 rm -r ./build
 
 # PRODUCTION: Build and zip Mainnet version
 npm run build:prod-mainnet
-cd ./build && zip -r ../ci/outputs/safe-browser-extension-$TRAVIS_BUILD_NUMBER-mainnet.zip * && cd ..
+cd ./build && zip -r ../ci/outputs/gnosis-safe-authenticator-$TRAVIS_BUILD_NUMBER-mainnet.zip * && cd ..
 rm -r ./build
